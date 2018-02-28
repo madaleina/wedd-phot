72ec0fb53df2e4c16e8634bcd52c338d69e3fd3c
meggu92@gmail.com <meggu92@gmail.com>
2018-02-28 17:54:12 +0100

    shadows

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,49 @@
-hi babe whats up
+(function($) {
+  "use strict"; // Start of use strict
 
-document.getElementsByClassName('js-scroll-trigger')
+  // handle links with @href started with '#' only
+  $(document).on('click', 'a[href^="#"]', function(e) {
+      // target element id
+      var id = $(this).attr('href');
+
+      // target element
+      var $id = $(id);
+      if ($id.length === 0) {
+          return;
+      }
+
+      // prevent standard hash navigation (avoid blinking in IE)
+      e.preventDefault();
+
+      // top position relative to the document
+      var pos = $id.offset().top;
+
+      // animated top scrolling
+      $('body, html').animate({scrollTop: pos});
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  })(jQuery); // End of use strict

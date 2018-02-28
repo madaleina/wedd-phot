84370ed763b4369e74ab5622f8ecb0166d91b0e0
meggu92@gmail.com <meggu92@gmail.com>
2018-02-28 23:40:20 +0100

    refresh

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,20 +28,29 @@
     offset: 56
   });
 
-  $(document).ready(function() {
+    $(document).ready(function() {
 
-      /* Every time the window is scrolled ... */
-      $(window).scroll( function(){
-         /* Check the location of each desired element */
-          $('.hide').each( function(i){
-              var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-              var bottom_of_window = $(window).scrollTop() + $(window).height();
-              /* If the object is completely visible in the window, fade it it */
-              if( bottom_of_window > bottom_of_object ){
-                  $(this).animate({'opacity':'1'},500);
-              }
+        /* Every time the window is scrolled ... */
+        $(window).scroll( function(){
+           /* Check the location of each desired element */
+            $('.hide').each( function(i){
+                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+                /* If the object is completely visible in the window, fade it it */
+                if( bottom_of_window > bottom_of_object ){
+                    $(this).animate({'opacity':'1'},500);
+                }
+            });
+        });
+    });
+
+    $(function(){
+      $('body').fadeIn(1000);
+      setTimeout(function(){
+          $('body').fadeOut(1000, function(){
+              location.reload(true);
           });
-      });
+      }, 60000);
   });
 
   // Collapse Navbar
